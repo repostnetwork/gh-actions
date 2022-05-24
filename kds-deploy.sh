ad689ab8c72d1d6e382ee088b384853da4d9245b
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2022-05-24 16:05:21 -0700

    trying some things

--- kds-deploy.sh
+++ kds-deploy.sh
@@ -26,8 +26,11 @@ if [ "$NEW_CLUSTER" = true ]; then
       --cache-cluster-id "${LOGICAL_NAME}-kds-dedup" \
       --show-cache-node-info | jq '.CacheClusters[0].ConfigurationEndpoint.Address')
 
+  echo "endpoint:"
+  echo "$CONFIG_ENDPOINT"
+
   end=$((SECONDS+300))
-  while [ $SECONDS -lt $end ] && [ -z "$CONFIG_ENDPOINT" ]; do
+  while [[ $SECONDS -lt $end  &&  -z "${CONFIG_ENDPOINT}" ]]; do
       echo "cluster not ready, retrying in 15s"
       sleep 15
       CONFIG_ENDPOINT=$(aws elasticache describe-cache-clusters \
